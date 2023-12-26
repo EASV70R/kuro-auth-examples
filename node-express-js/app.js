4da13402907208805738377d8555dfe39adfaab9
EASV70R <75667950+EASV70R@users.noreply.github.com>
2023-12-26 21:20:49 +0100

    server side security and log

--- node-express-js/app.js
+++ node-express-js/app.js
@@ -6,7 +6,27 @@ const port = 3000;
 
 const apiKey = 'APIKEY';
 
+const debug = false;
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`Connection attempt from IP: ${clientIp}`);
+    next();
+});
+
+app.use((req, res, next) => {
+    if (!debug) {
+        const secretHeader = req.headers['x-security-header'];
+        if (secretHeader !== 'key') {
+            const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+            console.error(`Failed connection attempt from IP: ${clientIp} due to invalid header`);
+            return res.status(403).json({ message: 'Invalid header' });
+        }
+    }
+    next();
+});
 
 app.get('/login', async (req, res) => {
     const { username, password } = req.query;
@@ -20,11 +40,12 @@ app.get('/login', async (req, res) => {
             }
         });
 
-        if (response.data.status === 'success') {
+        /*if (response.data.status === 'success') {
             res.send(`Login successful for user: ${response.data.data.username}`);
         } else {
             res.send(`Login failed: ${response.data.message}`);
-        }
+        }*/
+        res.json(response.data);
     } catch (error) {
         res.send(`Error: ${error.message}`);
     }
